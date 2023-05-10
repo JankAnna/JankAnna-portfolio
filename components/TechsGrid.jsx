683a132091759eb701271acb31e9e40ce0728c96
JankAnna <anzojank@gmail.com>
2023-05-10 00:22:36 -0400

    custom-67

--- components/TechsGrid.jsx
+++ components/TechsGrid.jsx
@@ -123,6 +123,7 @@ export const TitleSection = styled.h2`
 export default function TechsGrid(props) {
 	return (
 		<TechGrid>
+			<h2>Tytuł sekcji 1</h2>
 			<ScrollAnimation animateIn="fadeIn" animateOnce>
 				<Tech>
 					<object type="image/svg+xml" data="logo/logoINE.svg" class="logolight">
@@ -345,6 +346,49 @@ export default function TechsGrid(props) {
 				</Tech>
 			</ScrollAnimation>
 
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<Splunk />
+				</Tech>
+			</ScrollAnimation>
+
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<VisualStudio />
+				</Tech>
+			</ScrollAnimation>
+
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<Pycharm />
+				</Tech>
+			</ScrollAnimation>
+
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<Junipernetworks />
+				</Tech>
+			</ScrollAnimation>
+
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<Hackerone />
+				</Tech>
+			</ScrollAnimation>
+
+			<ScrollAnimation animateIn="fadeIn" animateOnce>
+				<Tech>
+					<Owasp />
+				</Tech>
+			</ScrollAnimation>
+		</TechGrid>
+	);
+}
+
+export default function TechsGrid(props) {
+	return (
+		<TechGrid>
+			<h2>Tytuł sekcji 2</h2>
 			<ScrollAnimation animateIn="fadeIn" animateOnce>
 				<Tech>
 					<Splunk />
